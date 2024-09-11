768570be5834b7936de261e810f4f6e0acb2ad92
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-09-11 13:28:36 -0300

    created consult page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'webTarefas';
+
+  categorias: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get('http://localhost:8080/api/categorias').subscribe({
+      next: (data) => {
+        this.categorias = data as any[];
+      }
+    })
+  }
 }
