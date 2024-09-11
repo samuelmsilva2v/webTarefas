3c0fe100c76f8b4e1c67e5d4c9c8d4fadfe98377
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-09-11 14:56:43 -0300

    implemented cadastrar and consultar tarefas

--- src/app/components/cadastrar-tarefa/cadastrar-tarefa.component.ts
+++ src/app/components/cadastrar-tarefa/cadastrar-tarefa.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environments } from '../../../environments/environment';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cadastrar-tarefa.component.html',
   styleUrl: './cadastrar-tarefa.component.css'
 })
@@ -16,10 +17,31 @@ export class CadastrarTarefaComponent {
 
   constructor(private httpClient: HttpClient) { }
 
+  form = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]),
+    data: new FormControl('', [Validators.required]), 
+    descricao: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(250)]),
+    categoriaId: new FormControl('', [Validators.required])
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+
   ngOnInit() {
     this.httpClient.get(environments.apiCategorias).subscribe({
       next: (data) => {
         this.categorias = data as any[];
+      }
+    })
+  }
+
+  onSubmit() {
+    this.httpClient.post(environments.apiTarefas, this.form.value, { responseType: 'text' }).subscribe({
+      next: (data) => {
+        alert(data);
+        this.form.reset();
       }
     })
   }
--- src/app/components/consultar-tarefas/consultar-tarefas.component.ts
+++ src/app/components/consultar-tarefas/consultar-tarefas.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environments } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-consultar-tarefas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './consultar-tarefas.component.html',
   styleUrl: './consultar-tarefas.component.css'
 })
 export class ConsultarTarefasComponent {
+  
+  tarefas: any[] = [];
 
+  constructor(private httpClient : HttpClient) { }
+
+  form = new FormGroup({
+    dataMin : new FormControl('', [Validators.required]),
+    dataMax : new FormControl('', [Validators.required])
+  })
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.httpClient.get(environments.apiTarefas + "/" + this.form.value.dataMin + "/" + this.form.value.dataMax).subscribe({
+      next: (data) => {
+        this.tarefas = data as any[];
+      }
+    })
+  }
 }
